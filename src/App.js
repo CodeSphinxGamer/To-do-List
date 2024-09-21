8a6d4b1b084789f39a3435aa28e31418e61731ee
Code Sphinx <bamboxgrt@gmail.com>
2024-09-21 00:24:51 -0400

    delete-complete

--- src/App.js
+++ src/App.js
@@ -7,21 +7,19 @@ import { CreateTodoButton } from "./CreateTodoButton";
 
 const defaultTodos = [
   { text: "Cortar cebolla", completed: true },
-  { text: "Tomar el Curso de Intro a React.js", completed: true },
+  { text: "Tomar el Curso de Intro a React.js", completed: false },
   { text: "Llorar con la Llorona", completed: false },
   { text: "LALALALALA", completed: false },
-  { text: "Usar estados Derivados", completed: false },
+  { text: "Usar estados derivados", completed: true },
+  { text: "prueba", completed: false },
 ];
 
 function App() {
   const [todos, setTodos] = React.useState(defaultTodos);
-
   const [searchValue, setSearchValue] = React.useState("");
 
   const completedTodos = todos.filter((todo) => !!todo.completed).length;
   const totalTodos = todos.length;
-
-  // const restTodos = todos.filter((todo) => !todo.completed).length;
 
   const searchedTodos = todos.filter((todo) => {
     const todoText = todo.text.toLowerCase();
@@ -29,11 +27,24 @@ function App() {
     return todoText.includes(searchText);
   });
 
-  console.log("Los usuarios buscan todos de: " + searchValue);
+  const completeTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex((todo) => todo.text == text);
+    newTodos[todoIndex].completed = true;
+    setTodos(newTodos);
+  };
+
+  const deleteTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex((todo) => todo.text == text);
+    newTodos.splice(todoIndex, 1);
+    setTodos(newTodos);
+  };
+
   return (
     <>
       <TodoCounter completed={completedTodos} total={totalTodos} />
-      <TodoSearch SearchValue={searchValue} setSearchValue={setSearchValue} />
+      <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
 
       <TodoList>
         {searchedTodos.map((todo) => (
@@ -41,6 +52,8 @@ function App() {
             key={todo.text}
             text={todo.text}
             completed={todo.completed}
+            onComplete={() => completeTodo(todo.text)}
+            onDelete={() => deleteTodo(todo.text)}
           />
         ))}
       </TodoList>
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,22 +1,33 @@
 import "./TodoItem.css";
+import { CompleteIcon } from "./CompleteIcon";
+import { DeleteIcon } from "./DeleteIcon";
 
 function TodoItem(props) {
   return (
     <div className="container">
       <li className="TodoItem">
-        <span
+        <CompleteIcon
+          completed={props.completed}
+          onComplete={props.onComplete}
+        />
+        {/* <span
           className={`Icon Icon-check ${
             props.completed && "Icon-check--active"
           }`}
+          onClick={props.onComplete}
         >
-          V
-        </span>
+          Pendiente..
+        </span> */}
         <p
           className={`TodoItem-p ${props.completed && "TodoItem-p--complete"}`}
         >
           {props.text}
         </p>
-        <span className="Icon Icon-delete">X</span>
+        <DeleteIcon onDelete={props.onDelete} />
+
+        {/* <span className="Icon Icon-delete" onClick={props.onDelete}>
+          X
+        </span> */}
       </li>
     </div>
   );
